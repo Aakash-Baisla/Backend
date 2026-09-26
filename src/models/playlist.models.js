90150089d88cca9662ce3e19b3e0c4279505a72c
Aakash Baisla <baislaakash5027@gmail.com>
2026-09-26 18:04:44 +0530

    Add comment,like,playlist and tweet models

---
+++ src/models/playlist.models.js
@@ -0,0 +1,24 @@
+import mongoose, {Schema} from "mongoose";
+
+const playlistSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    video : [
+        {
+        type: Schema.Types.ObjectId,
+        ref: "Video"
+    }
+],
+owner : {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+}
+},{timestampstrue})
+
+export const Playlist = mongoose.model("Playlist",playlistSchema)
